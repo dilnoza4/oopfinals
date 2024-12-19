195b93cba3b70f5605ce172b33cece2fa2a202b2
dilnoza.baidulloeva <dilnoza.baidulloeva_2027@ucentralasia.org>
2024-12-19 21:35:50 +0600

    Initial commit

---
+++ src/main/java/com/example/oopfinal/OopfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopfinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopfinal/OopfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
